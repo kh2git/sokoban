8d8f40d635ec1a100831a25bc6d7d6d073e277b9
Dok2gonzo <yhu8307@naver.com>
2019-06-05 16:10:02 +0900

    first commit

---
+++ sokoban.c
@@ -0,0 +1,154 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <termio.h>
+
+char name[10]; //이름저장소
+int mvcnt = 0; //움직임카운트
+int stage = 1; //스테이지 값
+char map[5][30][30] = 0;
+
+int getch(void)
+{
+	int ch;
+
+	struct termios buf;
+	struct termios save;
+
+	tcgetattr(0, &save);
+	buf = save;
+
+	buf.c_lflag &= ~(ICANON | ECHO);
+	buf.c_cc[VMIN] = 1;
+	buf.c_cc[VTIME] = 0;
+
+	tcsetattr(0, TCSAFLUSH, &buf);
+
+	ch = getchar();
+	tcsetattr(0, TCSAFLUSH, &save);
+
+	return ch;
+}
+
+int file_open
+{
+   FILE*fp;
+   fp = fopen("map.txt","rw");
+
+   while ((feof(fp)) != EOF)
+   {
+   fscanf(fp, "%s", map[i]);
+   i++;
+   }
+}
+
+char map[5][30][30] =
+{
+   {
+   맵1 들어갈자리
+   }
+
+   {
+   맵2 들어갈자리
+   }
+
+   {
+   맵3 들어갈자리
+   }
+
+   {
+   맵4 들어갈자리
+   }
+
+   {
+   맵5 들어갈자리
+   {
+
+   }
+}//맵12345
+
+
+void map_cnt() //맵박스개수체크
+{
+   int cnt1 = 0;
+   int cnt2 = 0;
+   for (int i = 0; i < 5,i++) {
+	  for (int j = 0; j < 30,i++) {
+		 for (int k = 0; k < 30,k++) {
+			if (map[i][j][k] == '$')
+			   cnt1++;
+			if (map[i][j][k] == 'O')
+			   cnt2++;
+		 }
+	  }
+	  if (cnt1 != cnt2) {
+
+	  printf("잘못된 맵입니다.");
+
+	  return 0;
+	  }
+	  cnt1 = 0;
+	  cnt2 = 0;
+   }
+
+}
+
+
+void inputname() //이름입력
+{
+
+   printf("이름 입력 : ");
+   scanf("%s",name[i]);
+}
+
+void map_print() //맵출력
+{
+	system(clear);
+
+	for (int i = 0; i < 30; i++) {
+		for (int j = 0; j < 30; j++) {
+			printf("%c",map[stage][i][j]);
+		}
+		printf("\n");
+	}
+}
+
+void inputkey(char input_char) //키보드입력
+{
+   switch (input_char)
+   case 'h':
+	x = -1
+		break;
+   case 'j':
+	y = -1
+	break;
+   case 'k':
+	y = 1;
+		break;
+   case 'l':
+	x = 1;
+	break;
+}
+
+
+void man() //d누르면 실행될 수 있도록 만들어야함//
+{
+   printf("창고지기 조정 : 왼쪽-h, 아래-j, 위-k, 오른쪽-l\n");
+   printf("가장 최근의 이동기록 취소(5번까지 사용가능 - u\n");
+   printf("현재 맵을 처음부터 다시시작 - r\n");
+   printf("첫번째 맵부터 다시시작 - n\n");
+   printf("게임 종료(자동저장) - e\n");
+   printf("현재 상태 파일에 저장 - s\n");
+   printf("save 지점부터 이어서 게임진행 - f\n");
+   printf("게임 순위 출력 - t\n");
+}
+
+void con()//단계별 완료시 축하메시지
+{
+   printf("congratulation");
+   return 0;
+}
+
+int main(void)
+{
+
+}
