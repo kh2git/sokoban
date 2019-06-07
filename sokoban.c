bd6d475d908cc997dcb4b8fa6f14da9ebdf6c29c
wisenick1 <51376082+wisenick1@users.noreply.github.com>
2019-06-08 02:28:33 +0900

    Update sokoban.c

--- sokoban.c
+++ sokoban.c
@@ -393,9 +393,19 @@ void inputkey(char input_char) //키보드입력
    case 'f':
 	
 	break;
-   case 'd':
-	
-	break;
+   void display() // display help
+   {
+     system("clear");
+     printf("h(왼쪽), j(아래), k(위), l(오른쪽)\n");
+     printf("u(undo)\n");
+     printf("r(replay)\n");
+     printf("n(new)\n");
+     printf("e(exit)\n");
+     printf("s(save)\n");
+     printf("f(file load)\n");
+     printf("d(display help\n");
+     printf("t(top)\n");
+}
 }
 
 
