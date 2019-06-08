06329d75edc50d2fe7ba864390a9374b1408c589
Kim Heehwan <51159078+kh2git@users.noreply.github.com>
2019-06-09 01:48:28 +0900

    Update sokoban.c

--- sokoban.c
+++ sokoban.c
@@ -518,18 +518,18 @@ void ranking_input()  //랭킹입력함수
 		fprintf(ofp, "%c\n\n",map[i]);
             	for (int m=0; m<=5; m++)
 		{
-			for (int j=0; j<=4; j++)
+			for (int j=5; j>0; j--)
 			{
-				if (score[i][j] > score[i][j+1])    //이동횟수 비교
+				if (score[i][j] < score[i][j-1])    //이동횟수 비교
 				{
 					temps = score[i][j];
-					score[i][j] = score[i][j+1];
-					score[i][j+1] = temps;
+					score[i][j] = score[i][j-1];
+					score[i][j-1] = temps;
 					for (int k=0; k<=9; k++)
 					{
 						tempn = names[i][j][k];
 						names[i][j][k] = names[i][j+1][k];
-						names[i][j+1][k] = tempn;
+						names[i][j-1][k] = tempn;
 					}
 				}
 			}
